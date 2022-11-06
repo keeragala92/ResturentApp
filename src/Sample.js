695d27bf9b99203b51b33c8b9ba0f8cc5f8e879d
Isuru Sampath <kasamee92@gmail.com>
2022-11-06 16:29:48 +0900

    New commit

---
+++ src/Sample.js
@@ -0,0 +1,26 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Sample = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <MaterialIcons name="home" size={30} color="#900" />
+      <Ionicons name="pizza" size={30} color="#900" />
+      <Text style={{fontFamily: 'Rubik-Bold', fontSize: 20}}>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Sample;
